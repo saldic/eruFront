1cddcb0935c9d2760a276cc6bc43a989104fbeac
cph-ds303 <cph-ds303@stud.ek.dk>
2026-06-04 23:49:29 +0200

    added required role for protected route (admin)

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,11 +1,15 @@
 import { Navigate, Outlet, useOutletContext } from "react-router";
 
-function ProtectedRoute() {
+function ProtectedRoute({ requiredRole }) {
   const outletContext = useOutletContext();
   const { currentUser } = outletContext;
 
   if (!currentUser) {
     return <Navigate to="/auth/login" replace />;
+  }
+
+  if (requiredRole && !currentUser.roles?.includes(requiredRole)) {
+    return <Navigate to="/feed" replace />;
   }
 
   return <Outlet context={outletContext} />;
